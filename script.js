9d876f4ebf5e1acc101af75752351194c21445ba
Andrew Frick <64029803+fosheezee@users.noreply.github.com>
2024-09-09 23:09:41 -0400

    content layout

---
+++ script.js
@@ -0,0 +1,10 @@
+const topLeft = document.getElementById('top-left');
+const topCenter = document.getElementById('top-center');
+const topRight = document.getElementById('top-right');
+const middleLeft = document.getElementById('middle-left');
+const middleCenter = document.getElementById('middle-center');
+const middleRight = document.getElementById('middle-right');
+const bottomLeft = document.getElementById('bottom-left');
+const bottomCenter = document.getElementById('bottom-center');
+const bottomRight = document.getElementById('bottom-right');
+
